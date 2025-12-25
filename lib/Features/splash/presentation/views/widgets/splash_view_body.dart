efd4b4db18356d120916e3881fc339a03d2af217
Ahmed Tamer <a.tamer7112002@gmail.com>
2025-12-25 18:42:18 +0200

    splash theming

--- lib/Features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/Features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nexus_book_app/constants.dart';
 import 'package:nexus_book_app/core/utils/assets/assets.dart';
 
 class SplashViewBody extends StatelessWidget {
@@ -8,10 +9,21 @@ class SplashViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width * 0.6,
-          height: MediaQuery.of(context).size.width * 0.6,
-          child: Image.asset(AssetsData.kLogo, fit: BoxFit.contain),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.6,
+              child: Image.asset(AssetsData.kLogo, fit: BoxFit.contain),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 13.0),
+              child: Text(
+                'Reed free books',
+                style: TextStyle(color: kSecondColor),
+              ),
+            ),
+          ],
         ),
       ),
     );
